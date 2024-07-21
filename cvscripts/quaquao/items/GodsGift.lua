ab76d9b508090281a8be3b6aeb1ecb5ea1a0cf86
quaquao <mysterblobski@gmail.com>
2024-07-21 19:05:43 +0200

    Update GodsGift.lua

--- cvscripts/quaquao/items/GodsGift.lua
+++ cvscripts/quaquao/items/GodsGift.lua
@@ -1,11 +1,9 @@
 local mod = CrossVerese
 
-local GodsGiftMoveSpeed = 0.50
+local GodsGiftMoveSpeed = +0.50
 
 function mod:UpdateGodsGiftSpeed(player)
 if player:HasCollectible(mod.collectible.item.GodsGift) then
-local health = player:GetSoulHearts() + 2 * player:GetBoneHearts() + player:GetMaxHearts()
 local SpeedToIncrease + GodsGiftMoveSpeed * player:GetCollectibleNum(mod.collectible.item.GodsGift)
 player.MoveSpeed = player.MoveSpeed + SpeedToIncrease
   end
-end
