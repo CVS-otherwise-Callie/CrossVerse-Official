6265f8dcb16e828c8961b7731c864523db33c1ee
quaquao <mysterblobski@gmail.com>
2024-07-22 08:39:53 +0200

    Update god's gift.lua

--- cvscripts/quaquao/items/god's gift.lua
+++ cvscripts/quaquao/items/god's gift.lua
@@ -7,6 +7,7 @@ function mod:UpdateGodsGiftMoveSpeed(player)
     local SpeedToIncrease = GodsGiftMoveSpeed * player:GetCollectibleNum(CollectibleType.GODS_GIFT)
     player.MoveSpeed = player.MoveSpeed + SpeedToIncrease
   end
+
   function mod:UpdateGodsGiftHealth(player)
    if player:HasCollectible(CollectibleType.GODS_GIFT) then
   local  player:AddSoulHearts(2)
