ffdd551a36f7ccdba766cea955574a53ddbec803
quaquao <mysterblobski@gmail.com>
2024-07-24 00:10:58 +0200

    Update constants.lua

--- cvscripts/constants.lua
+++ cvscripts/constants.lua
@@ -4,10 +4,10 @@ local mod = CrossVerse
 -- ITEMS --
 CollectibleType.COLLECTIBLE_GELATO = Isaac.GetItemIdByName("Gelato") --MAKE SURE THESE ARE IN ORDER TO XML I WILL WOOP YOUR ASS IF NOT id 1
 CollectibleType.COLLECTIBLE_DODGEBALL = Isaac.GetItemIdByName("Dodgeball") -- id 3
-CollectibleType.COLLECTIBLE_A_BALL = Isaac.GetItemIdByName("A Ball") -- id 6
-CollectibleType.GODS_GIFT = Isaac.GetItemIdByName("God's Gift") -- id 7
-CollectibleType.SKELETAL_FIGURE = Isaac.GetItemIdByName("Skeletal Figure") -- id 8
-CollectibleType.COLLECTIBLE_TOOTHPASTE = Isaac.GetItemIdByName("Toothpaste") -- id 9
+CollectibleType.COLLECTIBLE_A_BALL = Isaac.GetItemIdByName("A Ball") -- id 4
+CollectibleType.SKELETAL_FIGURE = Isaac.GetItemIdByName("Skeletal Figure") -- id 5
+CollectibleType.COLLECTIBLE_TOOTHPASTE = Isaac.GetItemIdByName("Toothpaste") -- id 6
+CollectibleType.GODS_GIFT = Isaac.GetItemIdByName("God's Gift") -- id 8
 
 mod.PassiveItems = {
     CollectibleType.COLLECTIBLE_GELATO,
