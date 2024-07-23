b7f66ed7e30c2575de69670ad278b8306339de3b
xocolatee <tolentino.elvis@gmail.com>
2024-07-23 14:32:06 +0100

    Delete cvscripts/cvscripts directory

--- cvscripts/cvscripts/der_existente/curses.lua
+++
@@ -1,7 +0,0 @@
---add silly swears and curses here that make no sense
-local mod = CrossVerse
-
-mod.der_existente.swears = {
-    "you fancy meat computer",
-    "your ass smells like a wet sandbag"
-}
