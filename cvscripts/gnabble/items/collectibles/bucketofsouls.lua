b07d86b0d1cf67400ae976f060e7edc0196924ce
gnabble <134325525+gnabble@users.noreply.github.com>
2024-07-25 20:40:30 -0400

    Update bucketofsouls.lua

--- cvscripts/gnabble/items/collectibles/bucketofsouls.lua
+++ cvscripts/gnabble/items/collectibles/bucketofsouls.lua
@@ -5,8 +5,9 @@ local USE_DELAY = 4
 local DURATION = 30 * 10
 
 ---@param entity Entity
-mod:AddCallback(ModCallbacks.MC_POST_ENTITY_KILL, function (_, entity)
-    if not PlayerManager.AnyoneHasCollectible(mod.Item.BUCKET_OF_SOULS) then return end
+---@param amt number
+mod:AddPriorityCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, CallbackPriority.LATE, function (_, entity, amt)
+    if not PlayerManager.AnyoneHasCollectible(mod.Item.BUCKET_OF_SOULS) or entity.SpawnerEntity or not entity:IsActiveEnemy(true) or amt < entity.HitPoints then return end
 
     for _, player in ipairs(PlayerManager.GetPlayers()) do
         if player:HasCollectible(mod.Item.BUCKET_OF_SOULS) then
@@ -21,14 +22,15 @@ end)
 ---@param player EntityPlayer
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, function (_, _, _, player)
     local data = mod.SaveManager.GetRunSave(player) if not data then return end
+
     data.BucketOfSouls = data.BucketOfSouls or {NUM_SOULS = 0}
 
     if data.BucketOfSouls.NUM_SOULS == 0 then return true end
 
     Isaac.CreateTimer(function ()
+        if #Isaac.FindByType(EntityType.ENTITY_EFFECT, EffectVariant.HUNGRY_SOUL, 1) >= MAX_SOULS then return end
         Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.HUNGRY_SOUL, 1, player.Position, RandomVector():Resized(10), player):ToEffect().Timeout = DURATION
         player:AnimateCollectible(mod.Item.BUCKET_OF_SOULS, "UseItem")
+        data.BucketOfSouls.NUM_SOULS = data.BucketOfSouls.NUM_SOULS - 1
     end, USE_DELAY, data.BucketOfSouls.NUM_SOULS, true)
-
-    data.BucketOfSouls.NUM_SOULS = 0
 end, mod.Item.BUCKET_OF_SOULS)
